6057755812ae2c7a105e66cf4ca678836b869b68
sobte <i@sobte.me>
2025-05-21 02:15:22 +0800

    feat: add server hardware

--- lib/host/default.nix
+++ lib/host/default.nix
@@ -18,7 +18,9 @@
     };
     # Fill in the key that all your hosts trust.
     # Note that they have large permissions and need to be saved offline.
-    authorizedKeys.keys = [ ];
+    authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICz8fLDHIDhM5rbjNfUMzeHpKQjGcruV7MzKwL4/08p6"
+    ];
     # starship config, see: https://starship.rs/config/
     starship.settings = builtins.fromTOML (builtins.readFile ./config/starship.toml);
   };
--- systems/x86_64-linux/hostname-server/default.nix
+++ systems/x86_64-linux/hostname-server/default.nix
@@ -2,7 +2,7 @@
   # If you have hardware.nix, please copy and import it.
   # You can use `nixos-generate-config` to generate
   # path: /etc/nixos/hardware-configuration.nix
-  # imports = [ ./hardware.nix ];
+  imports = [ ./hardware.nix ];
 
   cattery = {
     user.name = "root"; # use root as default user
